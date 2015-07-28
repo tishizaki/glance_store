c75bf81bc15324a1f3b878602de0cf009d6526b6
Teruaki Ishizaki <ishizaki.teruaki@lab.ntt.co.jp>
2015-07-28 11:20:37 +0900

    Fix some problem for test program
    This patch fixes python3 and 2.7 compatibility problems and
    change to replace dog command to fake command.
    
    Change-Id: I5e4e8067d25c3272d375eee621a41f013f9c16ea

--- glance_store/tests/unit/test_sheepdog_store.py
+++ glance_store/tests/unit/test_sheepdog_store.py
@@ -14,7 +14,7 @@
 #    under the License.
 
 import hashlib
-import StringIO
+from io import BytesIO
 
 import mock
 from oslo_concurrency import processutils
@@ -44,12 +44,15 @@ class TestSheepdogStore(base.StoreBaseTest,
         def _fake_execute(*cmd, **kwargs):
             pass
 
+        execute = mock.patch.object(processutils, 'execute').start()
+        self.addCleanup(execute.stop) 
+
         self.store = sheepdog.Store(self.conf)
         self.store.configure()
         self.image = sheepdog.SheepdogImage(SHEEP_ADDR, SHEEP_PORT,
                                             VDI_NAME, SHEEP_CHUNK_SIZE)
         self.called_commands = []
-        self.data = StringIO.StringIO('xx')
+        self.data = BytesIO(b'xx')
         self.loc = location.Location('test_sheepdog_store',
                                      sheepdog.StoreLocation,
                                      self.conf,
@@ -91,41 +94,38 @@ class TestSheepdogStore(base.StoreBaseTest,
         read_offset = 1
         read_count = 1
 
-        expected_cmd = ('read -s glance-image', None,
-                        str(read_offset), str(read_count))
+        expected_command = ('read -s glance-image', None,
+                            str(read_offset), str(read_count))
         self.image.read(read_offset, read_count)
-        fake_run_command.assert_called_once_with(*expected_cmd)
+        fake_run_command.assert_called_once_with(*expected_command)
 
     @mock.patch.object(sheepdog.SheepdogImage, '_run_command')
     def test_create_snapshot(self, fake_run_command):
-
+        expected_commands = ['snapshot -s glance-image']
         fake_run_command.side_effect = self._fake_run_command
         self.image.create_snapshot()
-        self.assertEqual(self.called_commands,
-                         ['snapshot -s glance-image'])
+        self.assertEqual(expected_commands, self.called_commands)
 
     @mock.patch.object(sheepdog.SheepdogImage, '_run_command')
     def test_delete_snapshot(self, fake_run_command):
-
+        expected_commands = ['delete -s glance-image']
         fake_run_command.side_effect = self._fake_run_command
         self.image.delete_snapshot()
-        self.assertEqual(self.called_commands, ['delete -s glance-image'])
+        self.assertEqual(expected_commands, self.called_commands)
 
     @mock.patch.object(sheepdog.SheepdogImage, '_run_command')
     def test_exist(self, fake_run_command):
-
         fake_run_command.side_effect = self._fake_run_command_true
         self.assertTrue(self.image.exist())
         self.assertEqual(self.called_commands, ['list -r'])
 
     @mock.patch.object(sheepdog.SheepdogImage, '_run_command')
     def test_add(self, fake_run_command):
-
+        expected_commands = ['list -r', 'create', 'write',
+                             'snapshot -s glance-image', 'delete']
         fake_run_command.side_effect = self._fake_run_command
         ret = self.store.add('fake_image_id', self.data, 2)
-        self.assertEqual(self.called_commands,
-                         ['list -r', 'create', 'write',
-                          'snapshot -s glance-image', 'delete'])
+        self.assertEqual(expected_commands, self.called_commands)
         self.assertEqual(('sheepdog://fake_image_id', 2,
                          hashlib.md5(self.data.getvalue()).hexdigest(),
                          {}), ret)
@@ -152,6 +152,7 @@ class TestSheepdogStore(base.StoreBaseTest,
 
     @mock.patch.object(sheepdog.SheepdogImage, 'exist')
     def test_add_image_already_exist(self, fake_exist):
+        expected_commands = ['exist']
 
         def _fake_exist():
             self.called_commands.append('exist')
@@ -160,15 +161,16 @@ class TestSheepdogStore(base.StoreBaseTest,
         exc = self.assertRaises(exceptions.Duplicate,
                                 self.store.add, 'fake_image_id',
                                 self.data, 2)
-        expect = "Image fake_image_id already exists"
-        self.assertEqual(expect, exc.msg)
-        self.assertEqual(self.called_commands, ['exist'])
+        expected_msg = "Image fake_image_id already exists"
+        self.assertEqual(expected_msg, exc.msg)
+        self.assertEqual(expected_commands, self.called_commands)
 
     @mock.patch.object(sheepdog.SheepdogImage, 'create')
     @mock.patch.object(sheepdog.SheepdogImage, '_run_command')
     @mock.patch.object(sheepdog, 'LOG')
     def test_add_image_create_fail(self, fake_logger, fake_run_command,
                                    fake_create):
+        expected_commands = ['list -r', 'create']
 
         def _fake_create(size):
             self.called_commands.append('create')
@@ -178,7 +180,7 @@ class TestSheepdogStore(base.StoreBaseTest,
         fake_create.side_effect = _fake_create
         self.assertRaises(exceptions.BackendException,
                           self.store.add, 'fake_image_id', self.data, 2)
-        self.assertEqual(self.called_commands, ['list -r', 'create'])
+        self.assertEqual(expected_commands, self.called_commands)
         self.assertTrue(fake_logger.error.called)
 
     @mock.patch.object(sheepdog.SheepdogImage, 'create_snapshot')
@@ -186,6 +188,8 @@ class TestSheepdogStore(base.StoreBaseTest,
     @mock.patch.object(sheepdog, 'LOG')
     def test_add_image_snapshot_fail(self, fake_logger, fake_run_command,
                                      fake_create_snapshot):
+        expected_commands = ['list -r', 'create', 'write',
+                             'snapshot -s glance-image', 'delete']
 
         def _fake_create_snapshot():
             self.called_commands.append('snapshot -s glance-image')
@@ -195,9 +199,7 @@ class TestSheepdogStore(base.StoreBaseTest,
         fake_create_snapshot.side_effect = _fake_create_snapshot
         self.assertRaises(exceptions.BackendException,
                           self.store.add, 'fake_image_id', self.data, 2)
-        self.assertEqual(self.called_commands,
-                         ['list -r', 'create', 'write',
-                          'snapshot -s glance-image', 'delete'])
+        self.assertEqual(expected_commands, self.called_commands)
         self.assertTrue(fake_logger.error.called)
 
     @mock.patch.object(sheepdog.SheepdogImage, 'delete')
@@ -205,6 +207,9 @@ class TestSheepdogStore(base.StoreBaseTest,
     @mock.patch.object(sheepdog, 'LOG')
     def test_add_image_delete_fail(self, fake_logger, fake_run_command,
                                    fake_delete):
+        expected_commands = ['list -r', 'create', 'write',
+                             'snapshot -s glance-image', 'delete',
+                             'delete -s glance-image']
         def _fake_delete():
             self.called_commands.append('delete')
             raise exceptions.BackendException()
@@ -213,10 +218,7 @@ class TestSheepdogStore(base.StoreBaseTest,
         fake_delete.side_effect = _fake_delete
         self.assertRaises(exceptions.BackendException,
                           self.store.add, 'fake_image_id', self.data, 2)
-        self.assertEqual(self.called_commands,
-                         ['list -r', 'create', 'write',
-                          'snapshot -s glance-image', 'delete',
-                          'delete -s glance-image'])
+        self.assertEqual(expected_commands, self.called_commands)
         self.assertTrue(fake_logger.error.called)
 
     def test_partial_get(self):
@@ -226,6 +228,7 @@ class TestSheepdogStore(base.StoreBaseTest,
     @mock.patch.object(sheepdog.SheepdogImage, 'exist')
     @mock.patch.object(sheepdog.SheepdogImage, 'delete_snapshot')
     def test_delete(self, fake_delete_snapshot, fake_exist):
+        expected_commands = ['exist', 'delete_snapshot']
 
         def _fake_exist_true():
             self.called_commands.append('exist')
@@ -237,7 +240,7 @@ class TestSheepdogStore(base.StoreBaseTest,
         fake_exist.side_effect = _fake_exist_true
         fake_delete_snapshot.side_effect = _fake_delete_snapshot
         self.store.delete(self.loc)
-        self.assertEqual(self.called_commands, ['exist', 'delete_snapshot'])
+        self.assertEqual(expected_commands, self.called_commands)
 
     @mock.patch.object(sheepdog.SheepdogImage, '_run_command')
     def test_delete_image_not_found(self, fake_run_command):
